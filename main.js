3dc0396d96e28ce13379b7b0c3e2a99e802a4301
hasanesmaeel777-gif <hasanesmaeel777@gmail.com>
2025-09-13 14:16:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let image = document.querySelector('.imge') ;
+let container = document.querySelector('.container') ;
+
+function phones(phone) {
+    image.src = phone ;
+}
+
+function colors(color) {
+    container.style.background = color ;
+}
